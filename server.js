0d464df5572dc102c7490fafcf376bae661a8b69
Patrick Panahon <patrick.panahon@gmail.com>
2017-10-16 16:52:27 -0400

    blah

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+if (require.main === module) {
+	app.listen(process.env.PORT || 8080);
+}
 
 module.exports = {app};
--- test/test-initial.js
+++ test/test-initial.js
@@ -3,17 +3,21 @@ const chaiHttp = require('chai-http');
 
 const should = chai.should();
 
-const {app} = require('../server');
+const {app, server} = require('../server');
 
 chai.use(chaiHttp);
 
 describe('initial web page', function() {
+
+	after(function() {
+		return process.exit();
+	});
+
 	it('should return a 200 status code and HTML when root URL accessed', function() {
 		return chai.request(app)
 			.get('/')
-			.then(function(_res) {
-				res = _res;
-				res.should.have.status(200);
+			.then(function(res) {
+				res.should.have.status(201);
 				res.should.be.html;
 			});
 	});
