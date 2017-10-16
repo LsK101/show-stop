4a207458382dbba0b48bb089f8e5a0a19e8f3d42
Patrick Panahon <patrick.panahon@gmail.com>
2017-10-16 14:48:40 -0400

    travis ci

---
+++ test/test-initial.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
+
+const {app} = require('../server');
+
+chai.use(chaiHttp);
+
+describe('initial web page', function() {
+	it('should return a 200 status code and HTML when root URL accessed', function() {
+		return chai.request(app)
+			.get('/')
+			.then(function(_res) {
+				res = _res;
+				res.should.have.status(200);
+				res.should.be.html;
+			});
+	});
+});
