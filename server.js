9c13c133b884afbfe7bad139d2669ecb83b195af
Patrick Panahon <patrick.panahon@gmail.com>
2017-10-18 16:57:44 -0400

    backend authentication working

--- server.js
+++ server.js
@@ -1,9 +1,88 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const {router: usersRouter} = require('./users');
+const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+
 const app = express();
+
 app.use(express.static('public'));
 
-if (require.main === module) {
-	app.listen(process.env.PORT || 8080);
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	if (req.method === 'OPTIONS') {
+		return res.send(204);
+	}
+	next();
+});
+
+app.use(passport.initialize());
+passport.use(basicStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+app.get('/api/protected', passport.authenticate('jwt', {session: false}),
+	(req, res) => {
+		return res.status(200).sendFile('./public/main.html', {root: __dirname});
+	}
+);
+
+//404 ANY UNKNOWN REQUESTS
+app.use('*', (req,res) => {
+	return res.status(404).json({message:'Not Found'});
+});
+
+
+
+// START/STOP SERVER HANDLING
+let server;
+
+function runServer() {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(DATABASE_URL, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(PORT, () => {
+				console.log(`Listening on port ${PORT}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
 }
 
-module.exports = {app};
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
---
+++ users/models.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+});
+
+UserSchema.methods.apiRepr = function() {
+	return {
+		username: this.username || ''
+	};
+};
+
+UserSchema.methods.validatePassword = function(password) {
+	return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+	return bcrypt.hash(password, 10);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
